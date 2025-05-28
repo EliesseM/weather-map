dabaa92114593754bf271aa498bbdc2ec6ea6666
mosbaheliesse <eliesse91@hotmail.fr>
2025-05-28 14:58:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Application WEATHER-MAP :
+Le but de cette application est d'afficher une carte sur laquelle nous pourrons zoomer et cliquer, le click sortira des coordonnées, ces coordonnées donneront ensuite la météo de cette zone via une API météo.
+
+etapes:
+initialisation du projet vite via cmd npm create vite@latest
+npm install
+npm rundev
+npm install maplibre-gl
+modification du index.html
+configuration de la carte dans le main.js
+
+initial commit
+creations des 4branches à partir de main (alex, Amrouche, Mosbah-Eliesse, guillaume).
---
+++ src/main.js
@@ -0,0 +1,39 @@
+import maplibre from "maplibre-gl";
+
+
+const map = new maplibre.Map({
+  container: "map",
+  center: [4.8522, 45.7566],
+  zoom: 10, // Niveau de zoom initial
+  minZoom: 0, // Zoom minimum autorisé
+  maxZoom: 18, // Zoom maximum autorisé
+  maxPitch: 50, // Inclinaison maximale de la carte
+
+  style: {
+    version: 8,
+    sources: {
+      osm: {
+        type: "raster",
+        tiles: ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+        tileSize: 256,
+        attribution: "&copy; OpenStreetMap",
+        maxzoom: 19,
+      },
+    },
+    layers: [
+      {
+        id: "osm",
+        type: "raster",
+        source: "osm",
+      },
+    ],
+  },
+});
+
+// The `click` event is an example of a `MapMouseEvent`.
+// Set up an event listener on the map.
+map.on("click", (e) => {
+  // The event object (e) contains information like the
+  // coordinates of the point on the map that was clicked.
+  console.log("A click event has occurred at " + e.lngLat);
+});
